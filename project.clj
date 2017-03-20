84b4687e086c5d4edc861f558ab06c1e5ef749e1
James <jmborden@gmail.com>
2017-03-20 12:28:48 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject threejs-game/lein-template "0.1.0"
+  :description "A Leiningen template for clojurescript game development using the three.js library"
+  :url "https://github.com/jborden/threejs-game-template"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :scm {:name "git"
+        :url "https://github.com/jborden/threejs-game-template"}
+  :eval-in-leiningen true)
