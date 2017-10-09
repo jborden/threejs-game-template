39bd90f5a62ae7bac6fddbcaac0085bf710a181c
James <jmborden@gmail.com>
2017-10-08 20:48:22 -0400

    removed template-game test dir

--- template-game/src/js/template_game.externs.js
+++
@@ -1,8 +0,0 @@
-// because cljsjs/three has its own non-exhaustive externs, any ones needed
-// in addition to those at: https://github.com/cljsjs/packages/blob/master/three/resources/cljsjs/three/common/three.ext.js
-// must be appened to the THREE object
-THREE.Camera.lookAt = function () {};
-THREE.Renderer.setSize = function () {};
-THREE.Renderer.domElement = {};
-THREE.Mesh.translateX = function () {};
-THREE.Mesh.translateY = function () {};
